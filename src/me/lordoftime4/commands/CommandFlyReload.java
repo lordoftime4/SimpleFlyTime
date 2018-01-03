5cfa01ec4a2fc2cd1bb64f2dc80e0e49ada83d8b
lordoftime4 <kevin.raschke.kr@gmail.com>
2018-01-03 13:44:48 -0700

    Update CommandFlyReload.java

--- src/me/lordoftime4/commands/CommandFlyReload.java
+++ src/me/lordoftime4/commands/CommandFlyReload.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.sisko.fly.Main;
+import me.lordoftime4.fly.Main;
 
 public class CommandFlyReload implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {

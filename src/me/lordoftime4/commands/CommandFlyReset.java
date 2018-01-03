b454d219117bb451855a11c7b05ecfac90057548
lordoftime4 <kevin.raschke.kr@gmail.com>
2018-01-03 13:45:28 -0700

    Update CommandFlyReset.java

--- src/me/lordoftime4/commands/CommandFlyReset.java
+++ src/me/lordoftime4/commands/CommandFlyReset.java
@@ -7,10 +7,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.sisko.fly.PlayerSaver;
-import me.sisko.uuid.UUIDFetcher;
+import me.lordoftime4.fly.PlayerSaver;
+import me.lordoftime4.uuid.UUIDFetcher;
 import net.md_5.bungee.api.ChatColor;
-import me.sisko.fly.Main;
+import me.lordoftime4.fly.Main;
 
 public class CommandFlyReset implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
